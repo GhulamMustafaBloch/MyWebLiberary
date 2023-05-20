c9f6cdc6f45287b15a2a122e6d43e1aeba4db868
GhulamMustafaBloch <ghulammustafabloch0302666@gmail.com>
2023-05-20 15:13:16 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import Home from './Home/Home';
 import About from './Home/Contact/About/About';
 import Contact from './Home/Contact/Contact';
 import Galry from './Home/Contact/About/Galry/Galry';
-import { BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import { BrowserRouter as Router,Routes,Route, BrowserRouter} from 'react-router-dom';
 import Footer from './Footer/Footer';
 import ShowContent from "./show_content_books/ShowContent"
 
 
 function App() {
   return (
+    <BrowserRouter basename='/MyWebLiberary'>
     <Router>
       <Header/>
       <Routes>
-        <Route path='/' element ={<Home/>} />
+        <Route path='/MyWebLiberary/' element ={<Home/>} />
         <Route path='/about' element ={<About/>} />
         <Route path='/contact' element ={<Contact/>} />
         <Route path='/galry' element ={<Galry/>} />
@@ -23,6 +24,7 @@ function App() {
       </Routes>
       <Footer/>
     </Router>
+    </BrowserRouter>
   );
 }
 export default App;
